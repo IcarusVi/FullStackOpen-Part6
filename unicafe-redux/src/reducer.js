ff08b69fd76d48cfad39f2f997838496c24c56bf
IcarusVi <zscyrus31@gmail.com>
2020-10-29 14:05:27 -0400

    debugging or 6.1 and 6.2

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -8,14 +8,22 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state.good + 1 
+      console.log('This is good')
+      return {
+        ...state,
+        good: state.good+1
+      }
     case 'OK':
       return state.ok + 1
     case 'BAD':
       return state.bad + 1
     case 'ZERO':
-      return state
-    default: return state
+      return state = {
+        good: 0, 
+        ok: 0,
+        bad: 0
+      }
+    default: return initialState
   }
   
 }
